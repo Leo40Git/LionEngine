cf8367bc32030f5c7d14f9c6111d6b5e37956015
Leo40Git <kfir40mailminer@gmail.com>
2018-03-23 15:26:40 +0300

    Add dummy AssetDB writer
    Change AssetDB specs
    Remove gradle wrapper

---
+++ src/main/java/com/letest/DummyAssetDBWriter.java
@@ -0,0 +1,119 @@
+package com.letest;
+
+import static leo.lionengine.assetdb.AssetDB.ASST_HEAD;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import javax.swing.JOptionPane;
+
+public class DummyAssetDBWriter {
+
+	public static void main(String[] args) {
+		try {
+			main0();
+		} catch (Exception e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Failed to write to file test.lad:\n" + e, "Writer failure",
+					JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		}
+		System.exit(0);
+	}
+
+	private static void main0() throws Exception {
+		File out = new File("test.lad");
+		FileOutputStream fos = null;
+		FileChannel ch = null;
+		try {
+			fos = new FileOutputStream(out);
+			ch = fos.getChannel();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Failed to open output stream to file test.lad", "Writer failure",
+					JOptionPane.ERROR_MESSAGE);
+		}
+		ByteBuffer strBuf = ByteBuffer.allocate(64);
+		ByteBuffer intBuf = ByteBuffer.allocate(Integer.BYTES);
+		/*writeHead:*/ {
+			// headBuf isn't required outside this
+			ByteBuffer headBuf = ByteBuffer.allocate(ASST_HEAD.length);
+			headBuf.put(ASST_HEAD);
+			headBuf.position(0);
+			ch.write(headBuf);
+			headBuf = null;
+		}
+		getIntegerFromUser(intBuf, "amount of groups in database");
+		ch.write(intBuf);
+		Integer groupNum = intBuf.getInt(0);
+		if (groupNum == 0) {
+			fos.close();
+			return;
+		}
+		for (int groupID = 0; groupID < groupNum; groupID++) {
+			getStringFromUser(strBuf, "name of group #" + groupID, 64);
+			ch.write(strBuf);
+			getIntegerFromUser(intBuf, "size of group #" + groupID);
+			ch.write(intBuf);
+			Integer groupSize = intBuf.getInt(0);
+			intBuf.clear();
+			intBuf.putInt(0xFFFFFFFF);
+			intBuf.position(0);
+			for (int assetID = 0; assetID < groupSize; assetID++) {
+				ch.write(intBuf); // asset type
+				intBuf.position(0);
+				getStringFromUser(strBuf, "name of asset #" + assetID, 64);
+				ch.write(strBuf);
+				ch.write(intBuf); // offset in chunk file
+				intBuf.position(0);
+				ch.write(intBuf); // compressed size
+				intBuf.position(0);
+				ch.write(intBuf); // uncompressed size
+				intBuf.position(0);
+			}
+		}
+		fos.close();
+	}
+
+	private static void getIntegerFromUser(ByteBuffer out, String message) {
+		out.clear();
+		String in = null;
+		while (true) {
+			in = JOptionPane.showInputDialog(null, String.format("Enter %s:", message), "1");
+			if (in == null)
+				continue;
+			if (in.isEmpty())
+				continue;
+			break;
+		}
+		Integer i = Integer.parseUnsignedInt(in);
+		out.putInt(i);
+		out.position(0);
+	}
+
+	private static void getStringFromUser(ByteBuffer out, String message, int length) {
+		out.clear();
+		String in = null;
+		while (true) {
+			in = JOptionPane.showInputDialog(null, String.format("Enter %s:", message), "");
+			if (in.length() > length)
+				continue;
+			break;
+		}
+		byte[] inB = new byte[length];
+		byte[] inBTemp = null;
+		try {
+			inBTemp = in.getBytes("ISO-8859-1");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		System.arraycopy(inBTemp, 0, inB, 0, inBTemp.length);
+		out.put(inB);
+		out.position(0);
+	}
+
+}
--- src/main/java/leo/lionengine/assetdb/AssetDB.java
+++ src/main/java/leo/lionengine/assetdb/AssetDB.java
@@ -12,19 +12,27 @@ import java.util.Map;
 
 public class AssetDB {
 
+	public static final byte[] ASST_HEAD = new byte[] { 'A', 'S', 'S', 'T' };
+
 	public static class AssetEntry {
+		private Integer type;
 		private String name;
 		private Integer chunkOffset;
 		private Integer sizeCompressed;
 		private Integer sizeUncompressed;
 
 		public AssetEntry(ByteBuffer buf) {
-			name = getString(buf, 0);
+			type = buf.getInt();
+			name = getString(buf, 4, 64);
 			System.out.println(name + ")");
-			buf.position(32);
+			buf.position(68);
 			chunkOffset = buf.getInt();
 			sizeCompressed = buf.getInt();
 			sizeUncompressed = buf.getInt();
+		}
+		
+		public Integer getType() {
+			return type;
 		}
 
 		public String getName() {
@@ -50,11 +58,12 @@ public class AssetDB {
 		FileInputStream fis = new FileInputStream(file);
 		FileChannel fc = fis.getChannel();
 		ByteBuffer bufNum = ByteBuffer.allocate(4);
-		ByteBuffer bufString = ByteBuffer.allocate(32);
-		ByteBuffer bufAsset = ByteBuffer.allocate(44);
+		ByteBuffer bufString = ByteBuffer.allocate(64);
+		ByteBuffer bufAsset = ByteBuffer.allocate(80);
 		fc.read(bufNum); // read magic string
 		bufNum.flip();
-		if (bufNum.get() != 'A' || bufNum.get() != 'S' || bufNum.get() != 'S' || bufNum.get() != 'T') {
+		byte[] head = bufNum.array();
+		if (!Arrays.equals(ASST_HEAD, head)) {
 			fis.close();
 			throw new IOException("Invalid database file: Magic string is wrong!");
 		}
@@ -76,7 +85,7 @@ public class AssetDB {
 		for (Integer groupId = 0; groupId < groupNum; groupId++) {
 			fc.read(bufString); // read group name
 			bufString.flip();
-			String groupName = getString(bufString, 0);
+			String groupName = getString(bufString, 0, 64);
 			bufString.clear();
 			fc.read(bufNum); // read group length
 			bufNum.flip();
@@ -98,18 +107,21 @@ public class AssetDB {
 		fis.close();
 	}
 
-	private static String getString(ByteBuffer buf2, int off) {
-		byte[] buf = buf2.array();
-		int l = 0;
-		while (l < buf.length && buf[l] > 0)
-			l++;
-		byte[] cbuf = Arrays.copyOf(buf, l);
+	private static String getString(ByteBuffer buf, int off, int len) {
+		if (len > buf.limit())
+			throw new IndexOutOfBoundsException(Integer.toUnsignedString(len));
+		byte[] cbuf = new byte[len];
+		buf.position(off);
+		for (int i = 0; i < len; i++)
+			cbuf[i] = buf.get();
+		String ret = null;
 		try {
-			return new String(cbuf, "UTF-8");
+			ret = new String(cbuf, "ISO-8859-1");
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		return null;
+		ret = ret.trim();
+		return ret;
 	}
 
 }
